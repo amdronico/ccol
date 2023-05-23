cf0f3b725f6abcb6b7288924f3263ddf157d0f36
Oscar Sierra <oscarsierracontreras@gmail.com>
2023-05-23 07:20:14 -0500

    Ajustes generales en card e iconería

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,16 @@ const User = require('../models/user');
 
 // Controlador para el inicio de sesión
 exports.login = (req, res) => {
-  // Lógica de autenticación y redirección
+     // Realiza la lógica de autenticación
+  // ...
+  
+  // Si el inicio de sesión es exitoso, devuelve un objeto con 'success' en true
+  if (true) {
+    return { success: true };
+  } else {
+    // Si hay un error en el inicio de sesión, devuelve un objeto con 'success' en false y un mensaje de error
+    return { success: false, error: 'Nombre de usuario o contraseña incorrectos' };
+  }
 };
 
 // Controlador para el registro de usuarios
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,58 @@
 const express = require('express');
 const qrcode = require('qrcode');
+const controller = require('../controllers/authController');
 const router = express.Router();
-
+//Ruta para renderizar el login de la aplicación
 router.get('/', (req, res) => {
-  res.render('index.njk');
+  res.render('login.njk');
+});
+
+// Ruta para manejar la solicitud de inicio de sesión
+router.post('/login', (req, res) => {
+  // Obtén los datos del formulario de inicio de sesión desde req.body
+  const { username, password } = req.body;
+
+  // Llama a la función login en tu controlador, pasando los datos necesarios
+  const result = controller.login(username, password);
+
+  // Haz algo con el resultado,
+  if (result.success) {
+    res.redirect('/dashboard');
+  } else {
+    res.render('login.njk', { error: result.error });
+  }
 });
 
 router.get('/dashboard', async (req, res) => {
-  
   try {
-
-    const IdUser="123456789"; // Datos para el código QR
+    // Datos para el código QR
+    const IdUser="123456789"; 
     const UserLogin="Pepito Perez"
-
+    // Genera el contenido para card
+    const cards = [
+      { image: '/assets/images/admin_users.png', title: 'Gestión de usuarios' },
+      { image: '/assets/images/abastecimiento.png', title: 'Abastecimiento' },
+      { image: '/assets/images/cortesproducto.png', title: 'Cortes de producto' },
+      { image: '/assets/images/asignacionremisiones.png', title: 'Asignación de remisiones' },
+      { image: '/assets/images/montacargas.png', title: 'Montacargas' },
+      { image: '/assets/images/tramitepedidos.png', title: 'Trámite de pedidos' },
+      { image: '/assets/images/embalaje.png', title: 'Embalaje' },
+      { image: '/assets/images/liquidacion.png', title: 'Liquidación' },
+      { image: '/assets/images/estadisticas.png', title: 'Estadísticas' }
+    ];
     // Generar el código QR
     const qrCodeDataURL = await qrcode.toDataURL(IdUser);
 
-    res.render('dashboard.njk', { CodeQr:qrCodeDataURL, UserLogin: UserLogin,IdUser: IdUser});
+    res.render('dashboard.njk', { CodeQr:qrCodeDataURL, UserLogin: UserLogin,IdUser: IdUser,cards});
+    
+
   } catch (error) {
     res.status(500).send('Error al generar el código QR');
   }
 });
+
+
+
+
 
 module.exports = router;
