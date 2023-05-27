6ec3eac02cc630fe09e40b875373e2e5cba3dd86
Oscar Sierra <oscarsierracontreras@gmail.com>
2023-05-27 12:56:43 -0500

    Llamado por ajax para las demás plantillas, ajustes generales de alertas

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -18,8 +18,8 @@
 //Mostrar mensaje de alerta.
 $(document).ready(function() {
 	$('#showAlert').on('click', function() {
-	  iziToast.show({
-		title: 'Notificación 1',
+	  iziToast.error({
+		title: 'Error',
 		message: 'Esta es la primera notificación',
 		position: 'topCenter',
 		timeout: 9000,
@@ -27,8 +27,8 @@ $(document).ready(function() {
 		messageColor: '#022A39'
 	  });
   
-	  iziToast.show({
-		title: 'Notificación 2',
+	  iziToast.warning({
+		title: 'Alerta',
 		message: 'Esta es la segunda notificación',
 		position: 'topCenter',
 		timeout: 9000,
@@ -36,7 +36,7 @@ $(document).ready(function() {
 		messageColor: '#022A39'
 	  });
   
-	  iziToast.show({
+	  iziToast.success({
 		title: 'Notificación 3',
 		message: 'Esta es la tercera notificación',
 		position: 'topCenter',
@@ -45,8 +45,16 @@ $(document).ready(function() {
 		messageColor: '#022A39'
 	  });
   
-	  iziToast.show({
+	  iziToast.info({
 		title: 'Notificación 4',
+		message: 'Esta es la cuarta notificación',
+		position: 'topCenter',
+		timeout: 9000,
+		titleColor: '#E15F32',
+		messageColor: '#022A39'
+	  });
+	  iziToast.show({
+		title: 'Notificación 5',
 		message: 'Esta es la cuarta notificación',
 		position: 'topCenter',
 		timeout: 9000,
@@ -66,5 +74,80 @@ $('#loginButton').on('click', function(event) {
   
 	// Realizar el envío del formulario utilizando jQuery
 	$('#loginForm').submit();
+  });
+
+//Función Logout
+$('#logAut').on('click', function() {
+	iziToast.question({
+	  timeout: false,
+	  close: false,
+	  overlay: true,
+	  displayMode: 'once',
+	  id: 'confirm',
+	  zindex: 999999999,
+	  title: '¿Deseas cerrar sesión?',
+	  message: 'Si continúas, se cerrará tu sesión actual.',
+	  position: 'center',
+	  buttons: [
+		['<button><b>Sí</b></button>', function(instance, toast) {
+		  window.location.href = '/'; // Ruta a remplazar
+		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+		}, true],
+		['<button>No</button>', function(instance, toast) {
+		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+		}]
+	  ],
+	});
+  });
+
+//Función con botón setting
+
+$('#setting').on('click', function() {
+	iziToast.show({
+	  id: 'settingToast',
+	  theme: 'dark',
+	  icon: 'fa fa-cog',
+	  title: 'Configuración',
+	  message: 'Selecciona una opción:',
+	  position: 'center',
+	  progressBarColor: '#022a39',
+	  buttons: [
+		['<button><i class="fa fa-user"></i> Perfil</button>', function(instance, toast) {
+		  // Acción para el botón "Perfil"
+		  // Agrega tu código aquí
+		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+		}],
+		['<button><i class="fa fa-lock"></i> Contraseña</button>', function(instance, toast) {
+		  // Acción para el botón "Contraseña"
+		  // Agrega tu código aquí
+		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+		}],
+		['<button><i class="fa fa-sign-out-alt"></i> Cerrar sesión</button>', function(instance, toast) {
+		  // Acción para el botón "Cerrar sesión"
+		  // Agrega tu código aquí
+		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+		}]
+	  ]
+	});
+  });
+
+  //Función boton admin user
+  $(document).ready(function() {
+	$('#cardButton').click(function() {
+	  var templatePath = $(this).data('template'); // Obtener la ruta de la plantilla desde el atributo 'data-template'
+	  var title = $('#cardButton .text-container').data('title');
+	  console.log(title);
+      
+	  // Realizar una llamada AJAX a tu servidor Express para renderizar la plantilla
+	  $.ajax({
+		url: '/render-template',
+		method: 'POST',
+		data: { template: templatePath },
+		success: function(response) {
+		  $('#templateContainer').html(response);
+		  $('#title').html(title);
+		}
+	  });
+	});
   });
   
--- routes/index.js
+++ routes/index.js
@@ -26,29 +26,38 @@ router.post('/login', (req, res) => {
 router.get('/dashboard', async (req, res) => {
   try {
     // Datos para el código QR
+
     const IdUser="123456789" 
     const UserLogin="Pepito Perez"
     const Alerts=4
     // Genera el contenido para card
+    const templateName = 'cards.njk';
+    const Title = 'Gestión Logística'
     const cards = [
-      { image: '/assets/images/admin_users.png', title: 'Gestión de usuarios' },
-      { image: '/assets/images/abastecimiento.png', title: 'Abastecimiento' },
-      { image: '/assets/images/cortesproducto.png', title: 'Cortes de producto' },
-      { image: '/assets/images/asignacionremisiones.png', title: 'Asignación de remisiones' },
-      { image: '/assets/images/montacargas.png', title: 'Montacargas' },
-      { image: '/assets/images/tramitepedidos.png', title: 'Trámite de pedidos' },
-      { image: '/assets/images/embalaje.png', title: 'Embalaje' },
-      { image: '/assets/images/liquidacion.png', title: 'Liquidación' },
-      { image: '/assets/images/estadisticas.png', title: 'Estadísticas' }
+      { image: '/assets/images/admin_users.png', title: 'Gestión de usuarios', template:'adminuser.njk'},
+      { image: '/assets/images/abastecimiento.png', title: 'Abastecimiento', template:'abastecimiento.njk' },
+      { image: '/assets/images/cortesproducto.png', title: 'Cortes de producto', template:'cortesproducto.njk' },
+      { image: '/assets/images/asignacionremisiones.png', title: 'Asignación de remisiones' , template:'asigancionremision.njk'},
+      { image: '/assets/images/montacargas.png', title: 'Montacargas', template:'montacargas.njk'},
+      { image: '/assets/images/tramitepedidos.png', title: 'Trámite de pedidos', template:'tramitepedidos.njk' },
+      { image: '/assets/images/embalaje.png', title: 'Embalaje' , template:'embalaje.njk'},
+      { image: '/assets/images/liquidacion.png', title: 'Liquidación', template:'liquidacion.njk' },
+      { image: '/assets/images/estadisticas.png', title: 'Estadísticas' , template:'estadisticas.njk'}
     ];
     // Generar el código QR
     const qrCodeDataURL = await qrcode.toDataURL(IdUser);
 
-    res.render('dashboard.njk', { CodeQr:qrCodeDataURL, UserLogin: UserLogin,IdUser: IdUser,Alerts:Alerts,cards});
+    res.render('dashboard.njk', { CodeQr:qrCodeDataURL, UserLogin: UserLogin,IdUser: IdUser,Alerts:Alerts,Title:Title,templateName:templateName,cards});
 
   } catch (error) {
     res.status(500).send('Error al generar el código QR');
   }
+});
+
+router.post('/render-template', (req, res) => {
+  const templatePath = req.body.template; // Obtener la ruta de la plantilla enviada desde el cliente
+  // Luego, envía el HTML renderizado al cliente como respuesta
+  res.render(templatePath);
 });
 
 
