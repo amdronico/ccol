b9a32a8754135f6fb5914f4ad5ec66d55214d98a
Oscar Sierra Contreras <osierra@marval.local>
2023-06-02 14:21:23 -0500

    ajuste lightbox para cerrar y guardar información

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -15,7 +15,78 @@
 
 })(jQuery);
 
-//Mostrar mensaje de alerta.
+
+$(document).ready(function() {
+
+
+//función acción login
+$('#loginButton').on('click', function(event) {
+	event.preventDefault(); // Prevenir la acción predeterminada del formulario
+  
+	// Obtener los valores de los campos del formulario
+	var username = $('#username').val();
+	var password = $('#password').val();
+  
+	// Realizar el envío del formulario utilizando jQuery
+	$('#loginForm').submit();
+  });
+
+	//Función Logout
+$('#logAut').on('click', function() {
+	iziToast.question({
+	  timeout: false,
+	  close: false,
+	  overlay: true,
+	  displayMode: 'once',
+	  id: 'confirm',
+	  zindex: 999999999,
+	  title: '¿Deseas cerrar sesión?',
+	  message: 'Si continúas, se cerrará tu sesión actual.',
+	  position: 'center',
+	  buttons: [
+		['<button><b>Sí</b></button>', function(instance, toast) {
+		  window.location.href = '/'; // Ruta a remplazar
+		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+		}, true],
+		['<button>No</button>', function(instance, toast) {
+		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+		}]
+	  ],
+	});
+  });
+ 
+  //Llamado alert botón setting
+
+	$('#setting').on('click', function() {
+		iziToast.show({
+			id: 'settingToast',
+			theme: 'dark',
+			icon: 'fa fa-cog',
+			title: 'Configuración',
+			message: 'Selecciona una opción:',
+			position: 'center',
+			progressBarColor: '#022a39',
+			buttons: [
+			['<button><i class="fa fa-user"></i> Perfil</button>', function(instance, toast) {
+				// Acción para el botón "Perfil"
+				// Agrega tu código aquí
+				instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+			}],
+			['<button><i class="fa fa-lock"></i> Contraseña</button>', function(instance, toast) {
+				// Acción para el botón "Contraseña"
+				// Agrega tu código aquí
+				instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+			}],
+			['<button><i class="fa fa-sign-out-alt"></i> Cerrar sesión</button>', function(instance, toast) {
+				// Acción para el botón "Cerrar sesión"
+				// Agrega tu código aquí
+				instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+			}]
+			]
+		});
+		});
+
+	//Mostrar mensaje de alerta.
 $(document).ready(function() {
 	$('#showAlert').on('click', function() {
 	  iziToast.error({
@@ -64,95 +135,25 @@ $(document).ready(function() {
 	});
   });
 
-//función acción login
-$('#loginButton').on('click', function(event) {
-	event.preventDefault(); // Prevenir la acción predeterminada del formulario
-  
-	// Obtener los valores de los campos del formulario
-	var username = $('#username').val();
-	var password = $('#password').val();
-  
-	// Realizar el envío del formulario utilizando jQuery
-	$('#loginForm').submit();
-  });
-
-//Función Logout
-$('#logAut').on('click', function() {
-	iziToast.question({
-	  timeout: false,
-	  close: false,
-	  overlay: true,
-	  displayMode: 'once',
-	  id: 'confirm',
-	  zindex: 999999999,
-	  title: '¿Deseas cerrar sesión?',
-	  message: 'Si continúas, se cerrará tu sesión actual.',
-	  position: 'center',
-	  buttons: [
-		['<button><b>Sí</b></button>', function(instance, toast) {
-		  window.location.href = '/'; // Ruta a remplazar
-		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-		}, true],
-		['<button>No</button>', function(instance, toast) {
-		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-		}]
-	  ],
-	});
-  });
-
-//Función con botón setting
-
-$('#setting').on('click', function() {
-	iziToast.show({
-	  id: 'settingToast',
-	  theme: 'dark',
-	  icon: 'fa fa-cog',
-	  title: 'Configuración',
-	  message: 'Selecciona una opción:',
-	  position: 'center',
-	  progressBarColor: '#022a39',
-	  buttons: [
-		['<button><i class="fa fa-user"></i> Perfil</button>', function(instance, toast) {
-		  // Acción para el botón "Perfil"
-		  // Agrega tu código aquí
-		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-		}],
-		['<button><i class="fa fa-lock"></i> Contraseña</button>', function(instance, toast) {
-		  // Acción para el botón "Contraseña"
-		  // Agrega tu código aquí
-		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-		}],
-		['<button><i class="fa fa-sign-out-alt"></i> Cerrar sesión</button>', function(instance, toast) {
-		  // Acción para el botón "Cerrar sesión"
-		  // Agrega tu código aquí
-		  instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-		}]
-	  ]
-	});
-  });
-
-
-  $(document).ready(function() {
+	//llamado renderización cards
 	$('.buttoncard').click(function() {
-	  var templatePath = $(this).data('template'); // Obtener la ruta de la plantilla desde el atributo 'data-template'
-	  var title = $(this).find('.text-container').data('title');
-	  var route = $(this).data('route');
-  
-	  // Realizar una llamada AJAX a tu servidor Express para renderizar la plantilla
-	  $.ajax({
-		url: route,
-		method: 'POST',
-		data: { template: templatePath },
-		success: function(response) {
-		  $('#templateContainer').html(response);
-		  $('#title').html(title);
-		}
+		var templatePath = $(this).data('template'); // Obtener la ruta de la plantilla desde el atributo 'data-template'
+		var title = $(this).find('.text-container').data('title');
+		var route = $(this).data('route');
+	
+		// Realizar una llamada AJAX a tu servidor Express para renderizar la plantilla
+		$.ajax({
+		  url: route,
+		  method: 'POST',
+		  data: { template: templatePath },
+		  success: function(response) {
+			$('#templateContainer').html(response);
+			$('#title').html(title);
+		  }
+		});
 	  });
-	});
-  });
 
-
-  $(document).ready(function() {
+	 //Llamar lightbox editar 
 	$(document).on('click', '.editarBtn', function() {
 		var id = $(this).closest('tr').find('.id').text();
 		var name = $(this).closest('tr').find('.name').text();
@@ -175,21 +176,29 @@ $('#setting').on('click', function() {
 		}
 	  });
 	});
+
+	//Cerrar Lightbox
+	$(document).on('click', '.cancelarBtn', function() {
+		var lightbox = $(this).closest('.lightbox'); // Encontrar el contenedor del lightbox
+		lightbox.fadeOut(); // Ocultar el lightbox
+	  });
+
+	//guardar info lightBox
+	$(document).on('click', '.guardarBtn', function() {
+		var idInterno = $('#id').val();
+		var nombre = $('#name').val();
+		var cargo = $('#ocupation').val();
+	    
+		// Realizar las acciones necesarias con los datos actualizados
 	
-	$('#cancelarBtn').click(function() {
-	  $('#editLightbox').fadeOut();
+		$('#editLightbox').fadeOut();
 	});
 	
-	$('#guardarBtn').click(function() {
-	  var id = $('#id').val();
-	  var name = $('#name').val();
-	  var ocupation = $('#ocupation').val();
-	
-	  // Realizar las acciones necesarias con los datos actualizados
-	
-	  $('#editLightbox').fadeOut();
-	});
   });
+
+
+
+  
   
   
 
